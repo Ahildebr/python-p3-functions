f4a23bf552f866acfdb1607d9844c1813015f68b
Aidan Hildebrand <blackbirdgt3rs@gmail.com>
2025-01-27 08:10:22 -0800

    Completes Lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
 def greet(name):
+    print(f"Hello, {name}!")
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
-def add(num1, num2):
+def add(num1,num2):
+    sum = int(num1) + int(num2)
+    return(sum)
     pass
 
 def halve(number):
+    number = float(number / 2)
+    return number
     pass
